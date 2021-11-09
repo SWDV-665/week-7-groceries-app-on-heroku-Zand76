f190ff398a13783c47129c7a017bdce75f3b6fc6
Zand76 <37522349+Zand76@users.noreply.github.com>
2021-11-08 23:15:56 -0500

    Edit items function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,40 +28,37 @@ export class HomePage {
       name: "Juice",
       quantity: 4
     },
-
   ];
 
   constructor(public navCtrl: NavController, public toastController: ToastController, public alertController: AlertController) {
   }
 
   //Removing Items from Grocery List
-  removeItem(item) {
-    let index=this.items.indexOf(item);
-
-    if(index > -1){
-      this.items.splice(index, 1);
-    }
-
-    //Notification of changes
-    console.log("Removing Item - ", item);
+  removeItem(item, index) {
+    console.log("Delete clicked - ", item, index);
     const toast = this.toastController.create({
       message: 'Removing Item - ' + item.name + "...",
-      duration: 3000
+      duration: 5000
     });
-
     toast.present();
+    this.items.splice(index, 1);
+  }
+
+  editItem(item, index) {
+    console.log("Edit clicked - ", item, index);
+    this.showEditItemPrompt(item, index);
   }
 
   //Adding Items to Grocery List
-  addItem(item) {
-    console.log("Adding Item - ");
+  addItem() {
+    console.log("Add clicked");
     this.showAddItemPrompt();
   }
 
   //Add items via alert form
   showAddItemPrompt() {
     const prompt = this.alertController.create({
-      title: "Add Groceries",
+      title: "Add Item",
       message: "Please enter item...",
       inputs: [
         {
@@ -75,16 +72,71 @@ export class HomePage {
       ],
       buttons: [
         {
-          text: 'add',
+          text: 'save',
           handler: item => {
-            console.log(item, ' added');
+            console.log('Save clicked', item.name);
+            const toast = this.toastController.create({
+              message: 'Adding Item - ' + item.name + "...",
+              duration: 5000
+            });
+            toast.present();
             this.items.push(item);
           }
         },
         {
           text: 'cancel',
           handler: item => {
-            console.log('Cancel clicked', item);
+            console.log('Cancel clicked');
+            const toast = this.toastController.create({
+              message: 'Canceling...',
+              duration: 5000
+            });
+            toast.present();
+          },
+        },
+      ]
+    });
+    prompt.present();
+  }
+  //Edit items via alert form
+  showEditItemPrompt(item, index) {
+    const prompt = this.alertController.create({
+      title: "Edit Item",
+      message: "Please edit item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          placeholder: 'quantity',
+          value: item.quantity
+        },
+      ],
+      buttons: [
+        {
+          text: 'save',
+          handler: item => {
+            console.log('Save clicked - ', item.name);
+            const toast = this.toastController.create({
+              message: 'Editing Item - ' + item.name + "...",
+              duration: 5000
+            });
+            toast.present();
+            this.items[index] = item;
+          }
+        },
+        {
+          text: 'cancel',
+          handler: item => {
+            console.log('Cancel clicked - ', item.name);
+            const toast = this.toastController.create({
+              message: 'Canceling...',
+              duration: 5000
+            });
+            toast.present();
           },
         },
       ]
