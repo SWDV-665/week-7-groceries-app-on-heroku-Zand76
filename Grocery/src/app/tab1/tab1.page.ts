4d4d5a208fba47d792118aae255b6cc9ee5170a6
Zand76 <37522349+Zand76@users.noreply.github.com>
2022-04-05 21:30:01 -0400

    Add Social Sharing Function

--- Grocery/src/app/tab1/tab1.page.ts
+++ Grocery/src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 import { GroceriesServiceService } from '../groceries-service.service';
 import { InputDialogServiceService } from '../input-dialog-service.service';
+// import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 
 @Component({
@@ -20,6 +21,7 @@ export class Tab1Page {
     public alertCtrl: AlertController,
     public dataService: GroceriesServiceService,
     public inputDialogService: InputDialogServiceService,
+    // public socialSharing: SocialSharing,
     ) {}
 
   loadItems() {
@@ -34,6 +36,25 @@ export class Tab1Page {
     });
     await toast.present();
     this.dataService.removeItem(index);
+  }
+
+  async shareItem(item, index) {
+    console.log("Sharing Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Sharing Item - ' + index + ' ...',
+      duration: 3000
+    });
+    await toast.present();
+
+    // let message = "Grocery Item - Name: " + item.name + " - Quantity: " + item.quantity;
+    // let subject = "Shared via Groceries app";
+    
+    // this.socialSharing.share(message, subject).then(() => {
+    //   // Sharing via email is possible
+    //   console.log("Shared successfully!");
+    // }).catch((error) => {
+    //   console.log("Error while sharing ", error);
+    // });
   }
 
   async editItem(item, index) {
